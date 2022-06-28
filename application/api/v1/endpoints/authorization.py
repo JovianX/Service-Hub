3c617b032a400b3e4f9ab171f22388da743ecbf7
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2022-06-28 14:06:39 +0300

    Added Github authentication provider.

--- application/api/v1/endpoints/authorization.py
+++ application/api/v1/endpoints/authorization.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from application.core.authentication import auth_backend
 from application.core.authentication import fastapi_users
+from application.core.authentication import github_client
 from application.core.authentication import google_client
 from application.core.configuration import settings
 from application.schemas.users import UserCreate
@@ -20,4 +21,7 @@ router.include_router(fastapi_users.get_reset_password_router())
 router.include_router(fastapi_users.get_verify_router(UserRead))
 
 google_router = fastapi_users.get_oauth_router(google_client, auth_backend, settings.SECRET)
-router.include_router(google_router, prefix='/google', tags=['oauth'])
+router.include_router(google_router, prefix='/google')
+
+github_router = fastapi_users.get_oauth_router(github_client, auth_backend, settings.SECRET)
+router.include_router(github_router, prefix='/github')
