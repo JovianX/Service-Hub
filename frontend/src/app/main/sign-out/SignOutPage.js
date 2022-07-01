604d52e8871320d2f842699758f1638d7adadf91
Roman Sologor <solohor.roman@gmail.com>
2022-07-01 15:48:43 +0300

    Update SignOutPage logo.

--- frontend/src/app/main/sign-out/SignOutPage.js
+++ frontend/src/app/main/sign-out/SignOutPage.js
@@ -15,11 +15,7 @@ const SignOutPage = () => {
     <div className='flex flex-col flex-auto items-center sm:justify-center min-w-0'>
       <Paper className='flex items-center w-full sm:w-auto min-h-full sm:min-h-auto rounded-0 py-32 px-16 sm:p-48 sm:rounded-2xl sm:shadow'>
         <div className='w-full max-w-320 sm:w-320 mx-auto sm:mx-0'>
-          <img
-            className='w-48 mx-auto'
-            src='assets/images/logo/logo.svg'
-            alt='logo'
-          />
+          <img className='w-[100px] mx-auto' src='assets/images/logo.png' alt='logo' />
 
           <Typography className='mt-32 text-4xl font-extrabold tracking-tight leading-tight text-center'>
             You have signed out!
@@ -28,6 +24,6 @@ const SignOutPage = () => {
       </Paper>
     </div>
   );
-}
+};
 
 export default SignOutPage;
