dfb701bd99bede93fa7c8af71c4a3db8257c5bd2
Roman Sologor <solohor.roman@gmail.com>
2022-07-01 14:38:49 +0300

    Remove the settings panel from layouts.

--- frontend/src/app/theme-layouts/layout1/Layout1.js
+++ frontend/src/app/theme-layouts/layout1/Layout1.js
@@ -9,21 +9,18 @@ import FuseSuspense from '@fuse/core/FuseSuspense';
 import AppContext from 'app/AppContext';
 import { selectFuseCurrentLayoutConfig } from 'app/store/fuse/settingsSlice';
 
-import SettingsPanel from '../shared-components/SettingsPanel';
-
 import FooterLayout1 from './components/FooterLayout1';
 import LeftSideLayout1 from './components/LeftSideLayout1';
 import NavbarWrapperLayout1 from './components/NavbarWrapperLayout1';
 import RightSideLayout1 from './components/RightSideLayout1';
 import ToolbarLayout1 from './components/ToolbarLayout1';
 
-const Root = styled('div')(({ theme, config }) => ({
+const Root = styled('div')(({ config }) => ({
   ...(config.mode === 'boxed' && {
     clipPath: 'inset(0)',
     maxWidth: `${config.containerWidth}px`,
     margin: '0 auto',
-    boxShadow:
-      '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
   }),
   ...(config.mode === 'container' && {
     '& .container': {
@@ -44,23 +41,10 @@ function Layout1(props) {
       {config.leftSidePanel.display && <LeftSideLayout1 />}
 
       <div className='flex flex-auto min-w-0'>
-        {config.navbar.display && config.navbar.position === 'left' && (
-          <NavbarWrapperLayout1 />
-        )}
+        {config.navbar.display && config.navbar.position === 'left' && <NavbarWrapperLayout1 />}
 
-        <main
-          id='fuse-main'
-          className='flex flex-col flex-auto min-h-full min-w-0 relative z-10'
-        >
-          {config.toolbar.display && (
-            <ToolbarLayout1
-              className={config.toolbar.style === 'fixed' && 'sticky top-0'}
-            />
-          )}
-
-          <div className='sticky top-0 z-99'>
-            <SettingsPanel />
-          </div>
+        <main id='fuse-main' className='flex flex-col flex-auto min-h-full min-w-0 relative z-10'>
+          {config.toolbar.display && <ToolbarLayout1 className={config.toolbar.style === 'fixed' && 'sticky top-0'} />}
 
           <div className='flex flex-col flex-auto min-h-0 relative z-10'>
             <FuseDialog />
@@ -70,16 +54,10 @@ function Layout1(props) {
             {props.children}
           </div>
 
-          {config.footer.display && (
-            <FooterLayout1
-              className={config.footer.style === 'fixed' && 'sticky bottom-0'}
-            />
-          )}
+          {config.footer.display && <FooterLayout1 className={config.footer.style === 'fixed' && 'sticky bottom-0'} />}
         </main>
 
-        {config.navbar.display && config.navbar.position === 'right' && (
-          <NavbarWrapperLayout1 />
-        )}
+        {config.navbar.display && config.navbar.position === 'right' && <NavbarWrapperLayout1 />}
       </div>
 
       {config.rightSidePanel.display && <RightSideLayout1 />}
