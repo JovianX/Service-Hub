78ede27cbd27acf278e53ed9a8081a6e9ebb2e8c
Roman Sologor <solohor.roman@gmail.com>
2022-07-21 19:14:30 +0300

    Fix releases delete API call.

--- frontend/src/app/api/releases.js
+++ frontend/src/app/api/releases.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 export const getReleasesList = async () => await axios.get('/api/v1/helm/release/list');
 
 export const deleteRelease = async (context_name, namespase, release_name) =>
-  await axios.delete('/api/v1/helm/release', {
+  await axios.delete(`/api/v1/helm/release/${release_name}`, {
     params: {
       context_name,
       namespase,
-      release_name,
     },
   });
