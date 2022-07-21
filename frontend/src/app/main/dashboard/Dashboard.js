c3dd37c446fc1eed0a6f54628dd5832281187868
Roman Sologor <solohor.roman@gmail.com>
2022-07-21 14:22:02 +0300

    Update dashboard layout.

--- frontend/src/app/main/dashboard/Dashboard.js
+++ frontend/src/app/main/dashboard/Dashboard.js
@@ -1,9 +1,8 @@
-import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import FusePageSimple from '@fuse/core/FusePageSimple';
 import {
   getChartsCount,
   getContextsCount,
@@ -17,25 +16,11 @@ import {
 import DashboardHeader from './DashboardHeader';
 import SummaryWidget from './SummaryWidget';
 
-const Root = styled(FusePageSimple)(({ theme }) => ({
-  '& .FusePageSimple-header': {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: `inset 0 0 0 1px  ${theme.palette.divider}`,
-  },
-}));
-
 const Dashboard = () => {
   const dispatch = useDispatch();
   const dashboardData = useSelector(selectCounts);
 
-  const {
-    releases,
-    repositories,
-    charts,
-    contexts,
-    unhealthy,
-    services,
-  } = dashboardData;
+  const { releases, repositories, charts, contexts, unhealthy, services } = dashboardData;
 
   useEffect(() => {
     dispatch(getReleasesCount());
@@ -61,35 +46,74 @@ const Dashboard = () => {
 
   const renderContent = () => {
     return (
-      <motion.div
-        className='grid p-12 pt-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 w-full min-w-0 p-24'
-        variants={container}
-        initial='hidden'
-        animate='show'
-      >
-        <motion.div variants={item} className='h-[108px]'>
-          <SummaryWidget dataKey='Services' dataValue={services} />
-        </motion.div>
-        <motion.div variants={item}>
-          <SummaryWidget dataKey='Unhealthy' dataValue={unhealthy} textColor='text-red-500' />
-        </motion.div>
-        <motion.div variants={item}>
-          <SummaryWidget dataKey='Releases' dataValue={releases} textColor='text-amber-500' />
-        </motion.div>
-        <motion.div variants={item}>
-          <SummaryWidget dataKey='Charts' dataValue={charts} textColor='text-teal-500' />
-        </motion.div>
-        <motion.div variants={item}>
-          <SummaryWidget dataKey='Repos' dataValue={repositories} textColor='text-orange-500' />
-        </motion.div>
-        <motion.div variants={item}>
-          <SummaryWidget dataKey='Contexts' dataValue={contexts} textColor='text-green-500' />
-        </motion.div>
-      </motion.div>
+      <div>
+        <div>
+          <Typography
+            component={motion.span}
+            initial={{ x: -20 }}
+            animate={{ x: 0, transition: { delay: 0.2 } }}
+            delay={300}
+            className='text-24 md:text-32 font-extrabold tracking-tight p-12'
+          >
+            Helm
+          </Typography>
+
+          <motion.div
+            className='grid p-12 pt-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 w-full min-w-0 p-24'
+            variants={container}
+            initial='hidden'
+            animate='show'
+          >
+            <motion.div variants={item}>
+              <SummaryWidget dataKey='Repositories' dataValue={repositories} textColor='text-orange-500' />
+            </motion.div>
+            <motion.div variants={item}>
+              <SummaryWidget dataKey='Unhealthy' dataValue={unhealthy} textColor='text-red-500' />
+            </motion.div>
+            <motion.div variants={item}>
+              <SummaryWidget dataKey='Releases' dataValue={releases} textColor='text-amber-500' />
+            </motion.div>
+            <motion.div variants={item}>
+              <SummaryWidget dataKey='Charts' dataValue={charts} textColor='text-teal-500' />
+            </motion.div>
+          </motion.div>
+        </div>
+
+        <div className='mt-36'>
+          <Typography
+            component={motion.span}
+            initial={{ x: -20 }}
+            animate={{ x: 0, transition: { delay: 0.2 } }}
+            delay={300}
+            className='text-24 md:text-32 font-extrabold tracking-tight p-12'
+          >
+            Kubernetes
+          </Typography>
+
+          <motion.div
+            className='grid p-12 pt-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 w-full min-w-0 p-24'
+            variants={container}
+            initial='hidden'
+            animate='show'
+          >
+            <motion.div variants={item} className='h-[108px]'>
+              <SummaryWidget dataKey='Services' dataValue={services} />
+            </motion.div>
+            <motion.div variants={item}>
+              <SummaryWidget dataKey='Contexts' dataValue={contexts} textColor='text-green-500' />
+            </motion.div>
+          </motion.div>
+        </div>
+      </div>
     );
   };
 
-  return <Root header={<DashboardHeader />} content={renderContent()} />;
+  return (
+    <div>
+      <DashboardHeader />
+      {renderContent()}
+    </div>
+  );
 };
 
 export default Dashboard;
