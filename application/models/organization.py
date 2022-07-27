4fb636ecd689c7c794d574aaf6ba5b5407ae6243
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2022-07-27 20:15:48 +0300

    Kubernetes configuration context deletion fix.

--- application/models/organization.py
+++ application/models/organization.py
@@ -6,13 +6,14 @@ from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 from application.db.base_class import Base
+from application.db.fields import MutableJSON
 
 
 class Organization(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     settings = Column(JSON, nullable=False, default={})
-    kubernetes_configuration = Column(JSON, nullable=False, default={'configuration': {}, 'metadata': {}})
+    kubernetes_configuration = Column(MutableJSON, nullable=False, default={'configuration': {}, 'metadata': {}})
     helm_home = Column(LargeBinary, nullable=True)
     members = relationship('User', back_populates='organization', lazy='joined')
     services = relationship('Service', back_populates='organization', lazy='joined')
