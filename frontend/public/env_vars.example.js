5709e0225586284ca85a9b0d940078c27c4e340c
Roman Sologor <solohor.roman@gmail.com>
2022-08-01 14:47:14 +0300

    Rename env file.

---
+++ frontend/public/env_vars.example.js
@@ -0,0 +1,8 @@
+// create env_vars.js file with the desired values for the fields
+//
+// window.___env_vars___ = {
+//   ABSOLUTE_API_HOSTNAME: 'localhost',
+//   API_PROTOCOL: 'http',
+//   API_PORT: 8000,
+// };
+
--- frontend/public/env_vars.js
+++
@@ -1,5 +0,0 @@
-window.___env_vars___ = {
-  ABSOLUTE_API_HOSTNAME: 'localhost',
-  API_PROTOCOL: 'http',
-  API_PORT: 8000,
-};
