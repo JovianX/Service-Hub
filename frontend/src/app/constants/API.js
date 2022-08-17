55153d9ef800028a91f3249ac0e5b84369b2dd18
Roman Sologor <solohor.roman@gmail.com>
2022-08-17 16:25:21 +0300

    Api config fix (#25)
    * Add backend API config.
    
    * Rename env file.
    
    * Update ABSOLUTE_API_HOST to one string
    
    * Update env example file.
    
    * Import env file into index file.
    
    * Add env subdir
    
    * remove default env example
    
    Co-authored-by: Arthur Berezin <rtpro1@gmail.com>

--- frontend/src/app/constants/API.js
+++ frontend/src/app/constants/API.js
@@ -1,7 +1,1 @@
-export const API_PORT = window?.___env_vars___?.API_PORT || 8000;
-
-export const ABSOLUTE_API_HOSTNAME = window?.___env_vars___?.ABSOLUTE_API_HOSTNAME || 'localhost';
-
-export const API_PROTOCOL = window?.___env_vars___?.API_PROTOCOL || 'http';
-
-export const ABSOLUTE_API_HOST = `${API_PROTOCOL}://${ABSOLUTE_API_HOSTNAME}:${API_PORT}`;
+export const ABSOLUTE_API_HOST = window?.___env_vars___?.API_URL || 'http://localhost:8000';
