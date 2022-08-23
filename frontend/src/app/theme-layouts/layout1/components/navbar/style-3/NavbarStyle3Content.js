69ed4a1cf1e1a928b2fba8cf0dc4928dd5f14910
Arthur Berezin <2787296+rtpro@users.noreply.github.com>
2022-08-23 11:09:05 +0300

    27 update color scheme (#38)
    * Update UI
    
    * Update colors

--- frontend/src/app/theme-layouts/layout1/components/navbar/style-3/NavbarStyle3Content.js
+++ frontend/src/app/theme-layouts/layout1/components/navbar/style-3/NavbarStyle3Content.js
@@ -108,7 +108,7 @@ function NavbarStyle3Content(props) {
       <Root className={clsx('flex flex-auto flex h-full', props.className)}>
         <ThemeProvider theme={contrastTheme}>
           <div id='fuse-navbar-side-panel' className='flex shrink-0 flex-col items-center'>
-            <img className='w-44 my-32' src='assets/images/logo-mini.png' alt='logo' />
+            <img className='w-44 my-32' src='assets/images/logo-mini-white.png' alt='logo' />
 
             <FuseScrollbars
               className='flex flex-1 min-h-0 justify-center w-full overflow-y-auto overflow-x-hidden'
