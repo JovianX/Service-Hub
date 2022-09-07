b108fc2eeb19ec05815aa1e90a257365d1e122fb
nastiachooo <nastiachooo@gmail.com>
2022-09-07 13:29:00 +0300

    AC/added loading button, error messages

--- frontend/src/app/main/repositories/RepositoriesTable.js
+++ frontend/src/app/main/repositories/RepositoriesTable.js
@@ -1,4 +1,6 @@
 import AddIcon from '@mui/icons-material/Add';
+import SaveIcon from '@mui/icons-material/Save';
+import LoadingButton from '@mui/lab/LoadingButton';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -12,7 +14,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import FuseLoading from '@fuse/core/FuseLoading';
@@ -25,20 +27,30 @@ import {
   getRepositoryList,
   selectIsRepositoriesLoading,
   selectRepositories,
+  selectErrorsInfo,
 } from 'app/store/repositorySlice';
 
 const RepositoriesTable = () => {
+  const inputRef = useRef(null);
   const dispatch = useDispatch();
   const repositoryData = useSelector(selectRepositories);
   const isLoading = useSelector(selectIsRepositoriesLoading);
+  const errorsInfo = useSelector(selectErrorsInfo);
 
-  useEffect(() => {
-    dispatch(getRepositoryList());
-  }, [dispatch]);
-
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [repositoryToDelete, setRepositoryToDelete] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(true);
+
+  const handleClickSaveButton = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!e.target.form?.name.value || !e.target.form?.url.value) {
+      setLoading(false);
+    }
+    inputRef.current.click();
+  };
 
   const toggleDeleteModalOpen = () => {
     setIsDeleteModalOpen(!isDeleteModalOpen);
@@ -65,16 +77,31 @@ const RepositoriesTable = () => {
     setOpen(true);
   };
   const handleClose = () => {
+    if (errorsInfo) {
+      setShowErrorMessage(false);
+    }
     setOpen(false);
   };
 
   const handleSubmitCreate = async (e) => {
     e.preventDefault();
-    setOpen(false);
     const repository = { name: e.target.name.value, url: e.target.url.value };
-    await dispatch(createRepository(repository));
-    await dispatch(getRepositoryList());
+    const data = await dispatch(createRepository(repository));
+    if (data.payload?.status === 'ERROR') {
+      setShowErrorMessage(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+      return false;
+    }
+    setLoading(false);
+    setOpen(false);
+    dispatch(getRepositoryList());
   };
+
+  useEffect(() => {
+    dispatch(getRepositoryList());
+  }, [dispatch]);
 
   if (isLoading) {
     return (
@@ -94,31 +121,40 @@ const RepositoriesTable = () => {
       <div className='w-full flex flex-col min-h-full'>
         <Paper className='h-full mx-24 rounded mt-12'>
           <FuseScrollbars className='grow overflow-x-auto'>
-            <TableContainer>
-              <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>URL</TableCell>
-                    <TableCell align='right'>Actions</TableCell>
-                  </TableRow>
-                </TableHead>
-
-                <TableBody>
-                  {repositoryData?.map((row) => (
-                    <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                      <TableCell align='left'>{row.name}</TableCell>
-                      <TableCell align='left'>{row.url}</TableCell>
-                      <TableCell align='right'>
-                        <Button onClick={() => handleDeleteRepository(row.name)} variant='text' color='error'>
-                          <FuseSvgIcon className='hidden sm:flex'>heroicons-outline:trash</FuseSvgIcon>
-                        </Button>
-                      </TableCell>
+            {repositoryData.length ? (
+              <TableContainer>
+                <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Name</TableCell>
+                      <TableCell>URL</TableCell>
+                      <TableCell align='right'>Actions</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  </TableHead>
+
+                  <TableBody>
+                    {repositoryData?.map((row) => (
+                      <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell align='left'>{row.name}</TableCell>
+                        <TableCell align='left'>{row.url}</TableCell>
+                        <TableCell align='right'>
+                          <Button onClick={() => handleDeleteRepository(row.name)} variant='text' color='error'>
+                            <FuseSvgIcon className='hidden sm:flex'>heroicons-outline:trash</FuseSvgIcon>
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            ) : (
+              <div className='text-center p-[20px]'>
+                <h2 className='mb-[12px] text-2xl'>No repositories yet</h2>
+                <p className='mb-[16px] text-xl'>
+                  The list of repositories is empty. To add a repositories, fill out the form above
+                </p>
+              </div>
+            )}
           </FuseScrollbars>
         </Paper>
       </div>
@@ -153,13 +189,24 @@ const RepositoriesTable = () => {
               <TextField name='url' type='text' required id='outlined-required' label='URL' margin='normal' fullWidth />
             </div>
           </DialogContent>
-          <DialogActions className='p-24'>
-            <Button className='mr-14' onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button type='submit' className='ml-12' variant='contained' color='primary' startIcon={<AddIcon />}>
-              Save
-            </Button>
+          <DialogActions className='p-24 justify-between'>
+            <div>{showErrorMessage && errorsInfo?.message && <p className='text-red'>{errorsInfo.message}</p>}</div>
+            <div className='flex'>
+              <Button className='mr-14' onClick={handleClose}>
+                Cancel
+              </Button>
+              <LoadingButton
+                color='primary'
+                onClick={handleClickSaveButton}
+                loading={loading}
+                loadingPosition='start'
+                startIcon={<SaveIcon />}
+                variant='contained'
+              >
+                Save
+              </LoadingButton>
+            </div>
+            <input ref={inputRef} type='submit' className='hidden' />
           </DialogActions>
         </form>
       </Dialog>
