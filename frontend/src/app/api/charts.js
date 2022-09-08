4b62117ebdcf761a34a8d13a60fc9190773eb250
nastiachooo <nastiachooo@gmail.com>
2022-09-08 13:45:11 +0300

    AC/deploy a release from the charts list

--- frontend/src/app/api/charts.js
+++ frontend/src/app/api/charts.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 const CHART_API_PATH = '/api/v1/helm/chart';
 
 export const getChartList = async () => await axios.get(`${CHART_API_PATH}/list`);
+export const chartInstall = async (chart) => await axios.post(`${CHART_API_PATH}/install`, chart);
--- frontend/src/app/main/charts/ChartsTable.js
+++ frontend/src/app/main/charts/ChartsTable.js
@@ -1,3 +1,13 @@
+import SaveIcon from '@mui/icons-material/Save';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { InputLabel, Select } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -5,25 +15,35 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/system';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import FuseLoading from '@fuse/core/FuseLoading';
 import FuseScrollbars from '@fuse/core/FuseScrollbars/FuseScrollbars';
-import { getChartList, selectIsChartsLoading, selectCharts } from 'app/store/chartsSlice';
+import { getChartList, chartInstall, selectIsChartsLoading, selectCharts } from 'app/store/chartsSlice';
+import { getClustersList, selectClusters } from 'app/store/clustersSlice';
 
 import { getSelectItemsFromArray, getUniqueKeysFromTableData } from '../../uitls';
 
 import ChartsFilters from './ChartsFilters';
 
 const ChartsTable = () => {
+  const inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   const [charts, setCharts] = useState([]);
+  const [chart, setChart] = useState({});
   const [repositories, setRepositories] = useState([]);
   const [selectedRepository, setSelectedRepository] = useState('all');
+  const [open, setOpen] = useState(false);
+  const [cluster, setCluster] = useState('');
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
   const dispatch = useDispatch();
   const chartData = useSelector(selectCharts);
   const isLoading = useSelector(selectIsChartsLoading);
+  const clusterData = useSelector(selectClusters);
 
   useEffect(() => {
     setCharts(chartData);
@@ -31,30 +51,76 @@ const ChartsTable = () => {
 
   useEffect(() => {
     dispatch(getChartList());
+    dispatch(getClustersList());
   }, [dispatch]);
 
   useEffect(() => {
     if (chartData?.length) {
       const uniqueRepositories = getUniqueKeysFromTableData(chartData, 'repository_name');
-
       const namespacesSelectOptions = getSelectItemsFromArray(uniqueRepositories);
-
       setRepositories(namespacesSelectOptions);
     }
   }, [chartData]);
 
   useEffect(() => {
     let filteredCharts = chartData;
-
     if (selectedRepository !== 'all') {
       filteredCharts = filteredCharts.filter((el) => el.repository_name === selectedRepository);
     }
-
     setCharts(filteredCharts);
   }, [selectedRepository]);
 
   const handleSelectedRepository = (event) => {
     setSelectedRepository(event.target.value);
+  };
+
+  // modal actions
+  const handleClickSaveButton = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const { chart_name, version, description, release_name, values, context_name } = e.target.form;
+    if (
+      !chart_name?.value ||
+      !version?.value ||
+      !description?.value ||
+      !release_name?.value ||
+      !values?.value ||
+      !context_name?.value
+    ) {
+      setLoading(false);
+    }
+    inputRef.current.click();
+  };
+
+  const handleSubmitInstall = async (e) => {
+    e.preventDefault();
+    const { chart_name, version, description, release_name, values, context_name } = e.target;
+    const chart = {
+      chart_name: chart_name.value,
+      version: version.value,
+      description: description.value,
+      release_name: release_name.value,
+      values: values.value,
+      context_name: context_name.value,
+    };
+    await dispatch(chartInstall(chart));
+    await setLoading(false);
+    await setOpen(false);
+    await dispatch(getChartList());
+  };
+
+  const handleGetChart = (name) => {
+    const data = chartData.find((item) => item.name === name);
+    setChart(data);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChangeSelect = (e) => {
+    setCluster(e.target.value);
   };
 
   if (isLoading) {
@@ -67,13 +133,14 @@ const ChartsTable = () => {
 
   return (
     <div className='w-full flex flex-col min-h-full'>
-      <ChartsFilters
-        repositories={repositories}
-        selectedRepository={selectedRepository}
-        setSelectedRepository={handleSelectedRepository}
-      />
-
-      <Paper className='h-full mx-12 rounded'>
+      <div className='mx-14 mt-14'>
+        <ChartsFilters
+          repositories={repositories}
+          selectedRepository={selectedRepository}
+          setSelectedRepository={handleSelectedRepository}
+        />
+      </div>
+      <Paper className='h-full mx-24 rounded'>
         <FuseScrollbars className='grow overflow-x-auto'>
           <TableContainer>
             <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
@@ -98,12 +165,122 @@ const ChartsTable = () => {
                     <TableCell align='left'>{row.application_version}</TableCell>
                     <TableCell align='left'>{row.repository_name}</TableCell>
                     <TableCell align='left'>{row.description}</TableCell>
-                    <TableCell align='left'>-</TableCell>
+                    <TableCell align='right'>
+                      <Button
+                        type='submit'
+                        className='mx-12'
+                        variant='contained'
+                        color='primary'
+                        onClick={() => handleGetChart(row.name)}
+                      >
+                        Deploy
+                      </Button>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
+          <Dialog open={open} onClose={handleClose} fullWidth maxWidth='sm'>
+            <form onSubmit={handleSubmitInstall}>
+              <DialogTitle className='bg-primary text-center text-white'>Deploy</DialogTitle>
+              <DialogContent className='pb-0'>
+                <div className='mt-14'>
+                  <TextField
+                    name='chart_name'
+                    type='text'
+                    required
+                    id='outlined-required'
+                    label='Chart Name'
+                    margin='normal'
+                    fullWidth
+                    defaultValue={chart.name}
+                  />
+                </div>
+                <TextField
+                  name='version'
+                  type='text'
+                  required
+                  id='outlined-required'
+                  label='Chart Version'
+                  margin='normal'
+                  fullWidth
+                  defaultValue={chart.application_version}
+                />
+
+                <TextField
+                  name='description'
+                  type='text'
+                  required
+                  id='outlined-required'
+                  label='Description'
+                  margin='normal'
+                  fullWidth
+                  defaultValue={chart.description}
+                />
+                <TextField
+                  name='release_name'
+                  type='text'
+                  required
+                  id='outlined-required'
+                  label='Release name'
+                  margin='normal'
+                  fullWidth
+                />
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl margin='normal' fullWidth>
+                    <InputLabel id='demo-simple-select-label'>Clusters</InputLabel>
+                    <Select
+                      name='context_name'
+                      labelId='demo-simple-select-label'
+                      id='demo-simple-select-autowidth'
+                      value={cluster}
+                      required
+                      label='Clusters'
+                      onChange={handleChangeSelect}
+                    >
+                      {clusterData.map((cluster) => (
+                        <MenuItem key={cluster.name} value={cluster.name}>
+                          {cluster.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+
+                <TextField
+                  name='values'
+                  id='outlined-multiline-static'
+                  label='Custom Values'
+                  required
+                  multiline
+                  minRows={5}
+                  maxRows={15}
+                  fullWidth
+                  margin='normal'
+                />
+              </DialogContent>
+              <DialogActions className='p-24 justify-between'>
+                <div>{showErrorMessage && errorsInfo?.message && <p className='text-red'>{errorsInfo.message}</p>}</div>
+                <div className='flex'>
+                  <Button className='mr-14' onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <LoadingButton
+                    color='primary'
+                    onClick={handleClickSaveButton}
+                    loading={loading}
+                    loadingPosition='start'
+                    startIcon={<SaveIcon />}
+                    variant='contained'
+                  >
+                    Save
+                  </LoadingButton>
+                  <input ref={inputRef} type='submit' className='hidden' />
+                </div>
+              </DialogActions>
+            </form>
+          </Dialog>
         </FuseScrollbars>
       </Paper>
     </div>
