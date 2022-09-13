ceb5c329f33c0b4e1d17f209aa56d6e91edd230b
Anastasiya Chuprey <82256209+NastiaChooo@users.noreply.github.com>
2022-09-13 09:59:17 +0300

    51-ui-add-repo (#53)
    * AC/UI add repo
    * AC/added loading button, error messages
    Co-authored-by: Arthur Berezin <rtpro1@gmail.com>

--- frontend/src/app/api/repositories.js
+++ frontend/src/app/api/repositories.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const REPOSITORY_API_PATH = '/api/v1/helm/repository';
 
 export const getRepositoryList = async () => await axios.get(`${REPOSITORY_API_PATH}/list`);
-
+export const createRepository = async (repository) => await axios.post(`${REPOSITORY_API_PATH}/add`, repository);
 export const deleteRepository = async (repository) => await axios.delete(`${REPOSITORY_API_PATH}/${repository}`);
--- frontend/src/app/store/repositorySlice.js
+++ frontend/src/app/store/repositorySlice.js
@@ -1,16 +1,40 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-import { getRepositoryList as getRepositoryListAPI, deleteRepository as deleteRepositoryAPI } from '../api';
+import {
+  getRepositoryList as getRepositoryListAPI,
+  createRepository as createRepositoryAPI,
+  deleteRepository as deleteRepositoryAPI,
+} from '../api';
 
 export const getRepositoryList = createAsyncThunk('repositories/getRepositoryList', async () => {
   try {
     const response = await getRepositoryListAPI();
-
     const data = await response.data;
-
     return data;
   } catch (e) {
     return [];
+  }
+});
+
+export const createRepository = createAsyncThunk('repositories/createRepository', async (repository) => {
+  try {
+    const response = await createRepositoryAPI(repository);
+    const data = await response.data;
+    return data;
+  } catch (e) {
+    console.log(e);
+    if (!e.response.data) {
+      const data = {
+        message: 'Oops, something went wrong',
+        status: 'ERROR',
+      };
+      return data;
+    }
+    const data = {
+      message: e.response.data.message,
+      status: 'ERROR',
+    };
+    return data;
   }
 });
 
@@ -27,6 +51,7 @@ const repositoriesSlice = createSlice({
   initialState: {
     isLoading: false,
     repositories: [],
+    errorsInfo: {},
   },
   reducers: {},
   extraReducers: {
@@ -42,9 +67,21 @@ const repositoriesSlice = createSlice({
       ...state,
       isLoading: false,
     }),
+    [createRepository.fulfilled]: (state, { payload }) => ({
+      ...state,
+      isLoading: false,
+      errorsInfo: payload,
+    }),
+    [createRepository.pending]: (state, { payload }) => ({
+      ...state,
+    }),
+    [createRepository.rejected]: (state, { payload }) => ({
+      ...state,
+    }),
   },
 });
 
+export const selectErrorsInfo = ({ repositories }) => repositories.errorsInfo;
 export const selectRepositories = ({ repositories }) => repositories.repositories;
 export const selectIsRepositoriesLoading = ({ repositories }) => repositories.isLoading;
 
