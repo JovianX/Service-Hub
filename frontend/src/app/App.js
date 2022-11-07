bc85508a15bd66181f0eca8e56dd696ef6b06e27
Anastasiya Chuprey <82256209+NastiaChooo@users.noreply.github.com>
2022-11-07 21:16:28 +0200

    165 some pages become 404 after build 1 (#172)
    * AC/fixed pages become 404 after build
    
    * AC/fixed the logic of the routes

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,6 +1,7 @@
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 import { SnackbarProvider } from 'notistack';
 import { useSelector } from 'react-redux';
 import rtlPlugin from 'stylis-plugin-rtl';
@@ -9,12 +10,14 @@ import BrowserRouter from '@fuse/core/BrowserRouter';
 import FuseAuthorization from '@fuse/core/FuseAuthorization';
 import FuseLayout from '@fuse/core/FuseLayout';
 import FuseTheme from '@fuse/core/FuseTheme';
+import { initialRoutes, adminRoutes, operatorRoutes } from 'app/configs/routesConfig';
 import settingsConfig from 'app/configs/settingsConfig';
 import { selectMainTheme } from 'app/store/fuse/settingsSlice';
 import { selectCurrentLanguageDirection } from 'app/store/i18nSlice';
 import { selectUser } from 'app/store/userSlice';
 import themeLayouts from 'app/theme-layouts/themeLayouts';
 
+import AppContext from './AppContext';
 import { AuthProvider } from './auth/AuthContext';
 import { ABSOLUTE_API_HOST } from './constants/API';
 import withAppProviders from './withAppProviders';
@@ -41,29 +44,47 @@ const App = () => {
   const langDirection = useSelector(selectCurrentLanguageDirection);
   const mainTheme = useSelector(selectMainTheme);
 
+  let userInfo = {};
+  const access_token = localStorage.getItem('jwt_access_token');
+  if (access_token) {
+    userInfo = jwtDecode(access_token);
+  }
+
+  const getRoutes = () => {
+    if (userInfo.user_role === 'operator') {
+      return operatorRoutes;
+    }
+    if (userInfo.user_role === 'admin') {
+      return adminRoutes;
+    }
+    return initialRoutes;
+  };
+
   return (
-    <CacheProvider value={createCache(emotionCacheOptions[langDirection])}>
-      <FuseTheme theme={mainTheme} direction={langDirection}>
-        <AuthProvider>
-          <BrowserRouter>
-            <FuseAuthorization userRole={user.role} loginRedirectUrl={settingsConfig.loginRedirectUrl}>
-              <SnackbarProvider
-                maxSnack={5}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-                classes={{
-                  containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
-                }}
-              >
-                <FuseLayout layouts={themeLayouts} />
-              </SnackbarProvider>
-            </FuseAuthorization>
-          </BrowserRouter>
-        </AuthProvider>
-      </FuseTheme>
-    </CacheProvider>
+    <AppContext.Provider value={{ routes: getRoutes() }}>
+      <CacheProvider value={createCache(emotionCacheOptions[langDirection])}>
+        <FuseTheme theme={mainTheme} direction={langDirection}>
+          <AuthProvider>
+            <BrowserRouter>
+              <FuseAuthorization userRole={user.role} loginRedirectUrl={settingsConfig.loginRedirectUrl}>
+                <SnackbarProvider
+                  maxSnack={5}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  classes={{
+                    containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
+                  }}
+                >
+                  <FuseLayout layouts={themeLayouts} />
+                </SnackbarProvider>
+              </FuseAuthorization>
+            </BrowserRouter>
+          </AuthProvider>
+        </FuseTheme>
+      </CacheProvider>
+    </AppContext.Provider>
   );
 };
 
