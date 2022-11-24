f394e71435c6d563861a0e8afd4566de7909285d
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2022-11-24 18:27:28 +0200

    Renamed Alembic migration to remove branching of migrations.

--- application/migrations/versions/2022-11-18_1528_altered_event_enums.py
+++ application/migrations/versions/2022-11-18_1528_altered_event_enums.py
@@ -2,7 +2,7 @@
 Altered event enums.
 
 Revision ID: 488fbe744acd
-Revises: f6ed2d61f1fb
+Revises: e8700095ac3e
 Create Date: 2022-11-16 15:28:18.096036
 """
 import sqlalchemy as sa
@@ -13,7 +13,7 @@ from constants.events import EventSeverityLevel
 
 # revision identifiers, used by Alembic.
 revision = '488fbe744acd'
-down_revision = 'f6ed2d61f1fb'
+down_revision = 'e8700095ac3e'
 branch_labels = None
 depends_on = None
 
