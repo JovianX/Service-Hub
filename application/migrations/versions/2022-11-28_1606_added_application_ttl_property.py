c146e2e17355560268457915904d4e14359e49bb
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2022-12-05 13:58:20 +0200

    Removed duplicating head in migrations tree.

--- application/migrations/versions/2022-11-28_1606_added_application_ttl_property.py
+++ application/migrations/versions/2022-11-28_1606_added_application_ttl_property.py
@@ -2,16 +2,15 @@
 Added Application.ttl property.
 
 Revision ID: 17ba98259c52
-Revises: e8700095ac3e
+Revises: 488fbe744acd
 Create Date: 2022-11-28 16:06:05.282750
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '17ba98259c52'
-down_revision = 'e8700095ac3e'
+down_revision = '488fbe744acd'
 branch_labels = None
 depends_on = None
 
