9908f0154f756775b15b5b5077a349f6b024e231
vadim-zabolotniy <34000376+vadim-zabolotniy@users.noreply.github.com>
2023-01-02 11:37:17 +0200

    User access tokens. (#218)
    * Added permanent JWT authentication backend.
    
    * Access token management.
    
    * Operators allowed to manage access tokens.
    
    * Added event logging during access token usage.
    
    * Fixed unserializeable datetime during access token creation.
    
    * Made access tokens personal only.
    
    * Removed creation event index from migration.
    
    * Clean up of permanent jwt implementation.

---
+++ application/migrations/versions/2022-12-21_1641_added_user_access_token_model.py
@@ -0,0 +1,46 @@
+"""
+Added user access token model.
+
+Revision ID: 288af7dbc2d6
+Revises: 17ba98259c52
+Create Date: 2022-12-21 16:41:18.668676
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '288af7dbc2d6'
+down_revision = '17ba98259c52'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    statuses = sa.Enum('active', 'disabled', name='access_token_statuses')
+    op.create_table(
+        'access_token',
+        sa.Column('id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('created_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
+        sa.Column('status', statuses, nullable=False),
+        sa.Column('expiration_date', sa.DateTime(), nullable=True),
+        sa.Column('comment', sa.String(), nullable=False),
+        sa.Column('user_id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('creator_id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('organization_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['creator_id'], ['user.id'], ),
+        sa.ForeignKeyConstraint(['organization_id'], ['organization.id'], ),
+        sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_access_token_id'), 'access_token', ['id'], unique=False)
+    op.create_index(op.f('ix_event_id'), 'event', ['id'], unique=False)
+    op.execute("ALTER TYPE event_categories ADD VALUE IF NOT EXISTS 'access_token'")
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_event_id'), table_name='event')
+    op.drop_index(op.f('ix_access_token_id'), table_name='access_token')
+    op.drop_table('access_token')
+    statuses = sa.Enum('active', 'disabled', name='access_token_statuses')
+    statuses.drop(op.get_bind())
---
+++ application/models/access_token.py
@@ -0,0 +1,41 @@
+"""
+User invitation model
+"""
+from uuid import uuid4
+
+from sqlalchemy import CheckConstraint
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from constants.access_tokens import AccessTokenStatuses
+from db.base_class import Base
+from db.fields import enum_column
+
+
+class AccessToken(Base):
+    """
+    User access token.
+
+    id - unique identifier.
+    created_at - date when access token was created.
+    status - status of access token.
+    comment - description for this access token.
+    user_id - identifier of User to which belongs the access token.
+    organization_id - identifier of the organization to which access token
+                      belongs.
+    """
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    status = Column(enum_column(AccessTokenStatuses), nullable=False)
+    expiration_date = Column(DateTime, nullable=True)
+    comment = Column(String, nullable=False, default='')
+    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='access_tokens', lazy='selectin')
+    organization_id = Column(Integer, ForeignKey('organization.id'), nullable=False)
+    organization = relationship('Organization', back_populates='access_tokens', lazy='selectin')
