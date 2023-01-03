592a11f51fc800c54c2356abad97e5cc3bce3686
ATomkivEX <andrii.t@expletech.com>
2023-01-03 15:36:41 +0200

    changed enabled, on_failure hook fields to selectors

---
+++ frontend/src/app/main/templates/TemplateModal/TemplateBuilder/HooksBuilder/HookSelectors/SelectorOfHookType.js
@@ -0,0 +1,33 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useEffect } from 'react';
+
+const TYPE_VALUES = ['kubernetes_job'];
+
+const SelectorOfHookType = ({ type, setType, typeValue, label, infoIsYamlValid, handleOnChangeHook }) => {
+  useEffect(() => {
+    setType('');
+    if (typeValue) {
+      setType(typeValue);
+    }
+  }, [typeValue]);
+
+  const handleChangeType = (e) => {
+    setType(e.target.value);
+    handleOnChangeHook(label, e.target.value);
+  };
+
+  return (
+    <FormControl size='small' required fullWidth disabled={!!infoIsYamlValid} className='col-span-2'>
+      <InputLabel>{label}</InputLabel>
+      <Select name='type' value={type || typeValue} label={label} onChange={handleChangeType}>
+        {TYPE_VALUES?.map((item) => (
+          <MenuItem key={item} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SelectorOfHookType;
