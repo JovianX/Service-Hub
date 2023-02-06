bf817b4d3d38e04f1cce33c431a4b02952e36884
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2023-02-06 12:22:30 +0200

    Lock of queueing a lot of same Procrastinate tasks and locking only singe task execution.

--- application/start-task-executor.sh
+++ application/start-task-executor.sh
@@ -2,7 +2,7 @@
 i=0
 while [ $i -le 5 ]; do
 
-procrastinate --app=services.procrastinate.application.procrastinate worker --concurrency=6 && break
+procrastinate --app=services.procrastinate.application.procrastinate worker --concurrency=6 --delete-jobs=successful && break
 
 let i=i+1
 sleep 2
